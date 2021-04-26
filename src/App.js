c40b97f916acef1a1bb237fa6e8ee194b29aaa84
Clayton Novotney <claytonnovotney@gmail.com>
2021-04-26 16:10:56 -0700

    minor changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ const App = () => {
             <h2>Info 474</h2>
             <h2>April 20th, 2021</h2>
             <h3>Exploratory Data Analysis, Assignment 2</h3>
+            <br></br>
+            <h2>Step 1: Data Selection</h2>
+            <h2>Step 2: Exploratory Visual Analysis</h2>
         </div>
     );
 };
