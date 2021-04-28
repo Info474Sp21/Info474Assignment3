1bc20655d014bd6800b4be12bccfd0d22496b44b
Clayton Novotney <claytonnovotney@gmail.com>
2021-04-27 20:17:25 -0700

    got the rest of my data sets in their with the first visualization

--- src/TeamSalary.js
+++ src/TeamSalary.js
@@ -1,40 +1,33 @@
 import React, { Component } from 'react'
 import './style.css'
-import * as d3 from "d3";
 import { useFetch } from "../hooks/useFetch"
-import { scaleLinear } from "d3-scale";
-import { extent, max, min, bin } from "d3-array";
-import { scale } from "vega";
+import vegaEmbed from 'vega-embed';
 
 
 function TeamSalaryVisual() {
-    const githubDataURL = 'https://raw.githubusercontent.com/Cnovotn/Info474Assignment2/main/data/MLBStats.csv'
+    const githubDataURL = 'https://raw.githubusercontent.com/Cnovotn/Info474Assignment2/main/data/FranchiseSalary.csv'
     const [data, loading] = useFetch(
         githubDataURL
     );
-    const dataSmallSample = data.slice(0,300);
-    const salaryMaxExtend = extent(dataSmallSample, (d) => {
-        return +d.salary;
-    })
-    const size = 500;
-    const margin = 20;
-    const yScale =  scaleLinear()
-    .domain(salaryMaxExtend)
-    .range([size - margin, size - 350])
-    _bins = bin()
-    salaryBins = _bins(
-        dataSmallSample.map((d) => {
-            return +d.salary;
-        })
-    )
-    console.log(salaryBins.map((bin, i) => {
-        console.log(i, bin);
-    }));
+
+    var vegaVariables = {
+        "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+        "description": "A simple bar chart looking at all of the MLB franchises salary for 2001.",
+        "title":"2001 Total Salarys Per MLB Franchise",
+        "data": {"values": data},
+        "mark": {"type": "bar", "cornerRadiusEnd": 1},
+        "encoding": {
+            "x": {"field": "franchise", "type": "nominal", "axis": {"labelAngle": -90}},
+            "y": {"field": "TeamSalary", "type": "quantitative"}
+        }
+    }
+    vegaEmbed('#team-salary', vegaVariables);
 
     return (
         <div>
             <div id="team-salary">
                 <p>{loading && "Loading the data!!!" }</p>
+                <svg width="600" height="500"></svg>
             </div>
             <p className="vis-description">Description for the team salarys of 2001</p>
         </div>
