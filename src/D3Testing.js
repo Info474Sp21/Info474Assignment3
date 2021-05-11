7ca1964813fabc9d2d5ca75e44bf1e6f0d2b1ec8
Clayton Novotney <claytonnovotney@gmail.com>
2021-05-11 14:15:55 -0700

    connected to the dataset

--- src/D3Testing.js
+++ src/D3Testing.js
@@ -3,10 +3,12 @@ import './style.css'
 import { useFetch } from "../hooks/useFetch"
 
 function D3Testing() {
+    const dataByYearURL = "https://raw.githubusercontent.com/Info474Sp21/Info474Assignment3/main/data/data_by_year_o.csv"
     const githubDataURL = 'https://raw.githubusercontent.com/Cnovotn/Info474Assignment2/main/data/franchiseBasedData.csv';
     const [data, loading] = useFetch(
-        githubDataURL
+        dataByYearURL
     );
+    console.log(data);
     return (
         <div>
             <h1 className="centered">Testing D3 Implementation</h1>
